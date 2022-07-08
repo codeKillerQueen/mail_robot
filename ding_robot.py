e0191dcfbd710d488e27364c498dd34bda7036d2
codeKillerQueen <97217962+codeKillerQueen@users.noreply.github.com>
2022-07-08 09:51:14 +0800

    Update ding_robot.py

--- ding_robot.py
+++ ding_robot.py
@@ -1,6 +1,6 @@
 from dingtalkchatbot.chatbot import DingtalkChatbot
 
 def ding(msg):
-    webhook ='https://oapi.dingtalk.com/robot/send?access_token=e4d08e96c1dc58e09b22f18e5805635c617588829858c628412be35204889db4'
+    webhook ='your dingding chat webhook'
     xiaoding = DingtalkChatbot(webhook)
-    xiaoding.send_text(msg='[runner]' + '\n' + msg, at_mobiles=[18280098532])
+    xiaoding.send_text(msg='[runner]' + '\n' + msg, at_mobiles=[your phone number])
